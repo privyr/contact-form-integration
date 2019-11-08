f5d108128cf4f638033b6f7ae31117794df75c96
Shashank Pareta <pareta@privyr.com>
2019-11-08 14:13:29 +0800

    added ninja form integration

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 }());
 
 const PrivyrGenericCfIntegration = require('./scripts/privyr-generic-cf-integration').default;
+const PrivyrNinjaCfIntegration = require('./scripts/privyr-ninja-cf-integration').default;
 
 module.exports = {
     run: function (config) {
         new PrivyrGenericCfIntegration(config);
+    },
+    run_ninja: function (config) {
+        new PrivyrNinjaCfIntegration(config);
     }
 };
